0e4c6007be565f89a695c0023e738706fb460c04
Joel Sommers <jsommers@colgate.edu>
2015-02-07 20:09:05 -0500

    Fix minimal spec for p3

--- spec/part3_spec.rb
+++ spec/part3_spec.rb
@@ -5,7 +5,7 @@ describe "Part 3 tests" do
   context "Anagram tests" do
     it "should be defined" do
       expect {
-        combine_anagrams "a b c"
+        combine_anagrams ["a", "b", "c"]
       }.not_to raise_error
     end
   end
